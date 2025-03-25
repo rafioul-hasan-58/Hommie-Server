9ae411498848cb33578c7766b333192ae33e3bae
Rafioul Hasan <rafioulhasan2@gmail.com>
2025-03-25 16:33:09 +0600

    listing crud operation added

--- src/app/modules/listing/listing.controller.ts
+++ src/app/modules/listing/listing.controller.ts
@@ -14,7 +14,50 @@ const createUserIntoDB = catchAsync(async (req: Request, res: Response) => {
         data: result
     })
 })
+const getAllListingFromDB = catchAsync(async (req: Request, res: Response) => {
+    const result = await listingService.getAllListingFromDB();
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Listings retrived successfully',
+        data: result
+    })
+})
+const getSingleListingFromDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await listingService.getSingleListingFromDB(id);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Single Listings retrived successfully',
+        data: result
+    })
+})
+const updateListingIntoDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await listingService.updateListingIntoDB(id, req.body);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Listing updated successfully',
+        data: result
+    })
+})
+const deleteListingFromDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await listingService.deleteListingFromDB(id);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Listing deleted successfully',
+        data: null
+    })
+})
 
-export const ListingContrller = {
-    createUserIntoDB
+export const listingController = {
+    createUserIntoDB,
+    getAllListingFromDB,
+    getSingleListingFromDB,
+    updateListingIntoDB,
+    deleteListingFromDB
 }
